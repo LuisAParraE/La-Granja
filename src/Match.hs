5812f25ac64265286636a540e077926f6d67c86d
LuisAParraE <alex_parra07@hotmail.com>
2022-06-12 20:31:33 -0400

    Función para comparar las palabras

--- src/Match.hs
+++ src/Match.hs
@@ -1,4 +1,10 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
 module Match where
+
+newtype Target = T [Char]
+
+newtype Guess = G [Char]
 
 data Match
   = Nada
@@ -10,3 +16,22 @@ instance Show Match where
   show Toro = "T"
   show Vaca = "V"
   show Nada = "-"
+
+--Funciones para verificar si coinciden las palabras
+checkGuess :: [Char] -> [Char] -> [Match]
+checkGuess [] _ = []
+checkGuess xs ys = lookToros (zip xs (repeat Nada)) (zip ys (repeat False)) [] []
+
+lookToros :: [(Char, Match)] -> [(Char, Bool)] -> [(Char, Match)] -> [(Char, Bool)] -> [Match]
+lookToros [] _ xs zs = lookVacas xs zs
+lookToros ((c, cs) : ys) ((t, ts) : ws) xs zs
+  | c == t && not ts = lookToros ys ws (xs ++ [(c, Toro)]) (zs ++ [(t, True)])
+  | otherwise = lookToros ys ws (xs ++ [(c, cs)]) (zs ++ [(t, ts)])
+
+lookVacas [] _ = []
+lookVacas ((x, y) : xs) ((t, ts) : (a, as) : (r, rs) : (g, gs) : (e, es) : ws)
+  | x == a && not as = Vaca : lookVacas xs ((a, True) : (r, rs) : (g, gs) : (e, es) : (t, ts) : ws)
+  | x == r && not rs = Vaca : lookVacas xs ((a, as) : (r, True) : (g, gs) : (e, es) : (t, ts) : ws)
+  | x == g && not gs = Vaca : lookVacas xs ((a, as) : (r, rs) : (g, True) : (e, es) : (t, ts) : ws)
+  | x == e && not es = Vaca : lookVacas xs ((a, as) : (r, rs) : (g, gs) : (e, True) : (t, ts) : ws)
+  | otherwise = y : lookVacas xs ((a, as) : (r, rs) : (g, gs) : (e, es) : (t, ts) : ws)
