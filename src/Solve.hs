266b95114516b2c74230fc097a498e5752ebdb4a
LuisAParraE <alex_parra07@hotmail.com>
2022-07-05 08:04:20 -0400

    palabras usadas

--- src/Solve.hs
+++ src/Solve.hs
@@ -17,12 +17,12 @@ playSolver :: IO ()
 playSolver = do
   allWords <- loadWords
   palabra <- randomSelect allWords
-  solveTheGame palabra 0 allWords
+  solveTheGame palabra 0 allWords []
 
 -- | Función que maneja el flujo del solver, muestra la palabra sugerida, y luego pide la pista.
 -- Si la pista no es completamente buena, procede a buscar una palabra a sugerir y se repite el proceso.
 -- Si se llega a más 6 turnos. Finaliza el programa.
-solveTheGame palabra turn allWords
+solveTheGame palabra turn allWords usedWords
   | turn < turnos = do
     putStr "La palabra sugerida es "
     hFlush stdout
@@ -44,7 +44,7 @@ solveTheGame palabra turn allWords
             putStrLn "Has sido un tramposo, la IA gana! :c"
             hFlush stdout
           else do
-            solveTheGame nextPalabra (turn + 1) allWords
+            solveTheGame nextPalabra (turn + 1) allWords (usedWords ++ [palabra])
   | turn >= turnos = do
     putStrLn ""
     hFlush stdout
